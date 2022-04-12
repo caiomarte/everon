41e719da22436b0503d4a2d7e8006fe5f07f6640
Caio Martinho <caiomartesilva@gmail.com>
2022-04-12 04:27:31 -0300

    final_version

--- application/release.locals.tf
+++ application/release.locals.tf
@@ -6,7 +6,7 @@ locals {
     replicas  = var.application.replicas
     port      = var.application.port
     address   = var.cluster_endpoint
-    domain    = google_dns_managed_zone.zone.dns_name
+    domain    = trimsuffix(google_dns_managed_zone.zone.dns_name, ".")
     manager   = "terraform"
     author    = "${lower(var.author.name)}-${lower(var.author.surname)}"
   }
